40253c8f42576bbe01fdf33c248ab53d7a269a92
elisangeladesouza <elysangeladesouza@gmail.com>
2017-07-24 15:52:57 -0300

    Criação do projeto e inserção das dependências.

---
+++ src/main/java/br/com/gestao/livraria/GestaoLivrariaApplication.java
@@ -0,0 +1,12 @@
+package br.com.gestao.livraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoLivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoLivrariaApplication.class, args);
+	}
+}
